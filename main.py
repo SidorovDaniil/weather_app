7fdccacaebfadad3d3c6cc2ba50c67535b44c4ee
SidorovDaniil <svdenielvs@gmail.com>
2023-12-05 21:29:26 +0300

    Added time definition

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import geocoder
 import pandas as pd
 import os
-
+from datetime import datetime, timedelta
 
 API_KEY = 'b9223c17622bebeec90cc05201230585'
 LINK = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&q={}&appid={}'
@@ -14,14 +14,27 @@ TEXT_OF_INTERFACE = "Что вы хотите узнать?\n" \
                     "Введите '4' чтобы очистить историю\n" \
                     "Введите '5' чтобы выйти из программы\n"
 
-TEXT_OF_WEATHER_REPORT = "Город: {}\n" \
+TEXT_OF_WEATHER_REPORT = "Текущее время: {}\n" \
+                         "Город: {}\n" \
                          "Температура: {}°C\n" \
                          "Ощущается как: {}°C\n" \
                          "Облачность: {}\n" \
                          "Скорость ветра: {} м/с\n"
 
 
-def get_user_city() -> dict[str, float]:
+def get_weather_data_for_city(city: str) -> dict[str: any]:
+    """
+    Совершает запрос к веб сервису для определения погоды
+
+    :param city: Город, погодные данные которого требуется определить
+    :return: словарь с погодными данными для города, введенного пользователем
+    """
+
+    response = requests.get(LINK.format(city, API_KEY))
+    return response.json()
+
+
+def get_user_city() -> str:
     """
     Определяет город пользователя по его ip
 
@@ -31,36 +44,39 @@ def get_user_city() -> dict[str, float]:
     return geocoder.ip('me').city
 
 
-def get_weather_data_for_city(city) -> dict[str: any]:
-    """
-    Совершает запрос к веб сервису для определения погоды
+def get_time(weather_data) -> str:
 
-    :return: словарь с погодными данными для города, введенного пользователем
-    """
+    timestamp = weather_data['dt']
+    timezone = weather_data['timezone']
 
-    response = requests.get(LINK.format(city, API_KEY))
-    return response.json()
+    current_time = f'{datetime.fromtimestamp(timestamp)} + {timedelta(seconds=timezone)}'
+
+    return current_time
 
 
-def parse_weather_data(weather_data) -> dict[str: any]:
+def parse_weather_data(weather_data: dict[str: any]) -> dict[str: any]:
     """
     Функция парсит из словаря следующие данные:
+        Текущее время current_time
         Город, где были произведены измерения city
         Температура temperature
         Ощущение температуры temp_feels_like
         Облачность cloud
         Скорость ветра wind_speed
 
-    :return: словарь с необходимыми данными
+    :param weather_data: Словарь со всевозможными данными о погоде
+    :return: Словарь с необходимыми данными
     """
 
+    current_time = get_time(weather_data)
     city = weather_data['name']
     temperature = weather_data['main']['temp']
     temp_feels_like = weather_data['main']['feels_like']
     cloud = weather_data['weather'][0]['description']
     wind_speed = weather_data['wind']['speed']
 
-    useful_weather_data = {'city': city,
+    useful_weather_data = {'current_time': current_time,
+                           'city': city,
                            'temp': temperature,
                            'feels_like': temp_feels_like,
                            'cloud': cloud,
@@ -77,7 +93,8 @@ def weather_report(useful_weather_data: dict[str, any]):
     :return: Отформатированная строка с данными
     """
 
-    return TEXT_OF_WEATHER_REPORT.format(useful_weather_data['city'],
+    return TEXT_OF_WEATHER_REPORT.format(useful_weather_data['current_time'],
+                                         useful_weather_data['city'],
                                          useful_weather_data['temp'],
                                          useful_weather_data['feels_like'],
                                          useful_weather_data['cloud'],
@@ -95,7 +112,7 @@ def create_or_read_history() -> pd.DataFrame:
     if os.path.exists('history.csv'):
         return pd.read_csv('history.csv')
 
-    return pd.DataFrame(columns=['city', 'temp', 'feels_like', 'cloud', 'wind_speed'])
+    return pd.DataFrame(columns=['current_time', 'city', 'temp', 'feels_like', 'cloud', 'wind_speed'])
 
 
 def write_data_to_history(weather_data: dict[str, any]) -> None:
@@ -105,33 +122,35 @@ def write_data_to_history(weather_data: dict[str, any]) -> None:
     :param weather_data: Словарь с данными, которые необходимо записать в историю
     :return: None
     """
+
     history = create_or_read_history()
     history.loc[len(history)] = weather_data
     history.to_csv('history.csv', index=False)
 
 
-def see_last_n_requests(n: int) -> None:
+def see_last_n_requests(number_of_requests: int) -> None:
     """
     Функция возвращает n последних запросов из истории, если это возможно
 
-    :param n: Целое число, равно количеству запросов, которые желает увидеть пользователь
+    :param number_of_requests: Целое число, равно количеству запросов, которые желает увидеть пользователь
     :return: None
     """
+
     history = create_or_read_history()
 
-    if n == 0:
+    if number_of_requests == 0:
         print('\nВыведено 0 запросов, как вы и просили\n')
 
-    elif n < 0:
+    elif number_of_requests < 0:
         print(f'\nЧисло n должно быть положительно\n')
 
-    elif n > history.shape[0]:
+    elif number_of_requests > history.shape[0]:
         print(f'\nВ истории нет такого количества запросов\nПопробуйте ввести число от 0 до {history.shape[0]}\n')
 
     else:
-        print(f'\nВыведено {n} запросов\n')
+        print(f'\nВыведено {number_of_requests} запросов\n')
         print('_______________________________')
-        for i in range(1, n+1):
+        for i in range(1, number_of_requests+1):
             row = history.iloc[-i, :].to_dict()
             weather_data = weather_report(row)
 
@@ -145,6 +164,7 @@ def delete_history() -> None:
 
     :return: None
     """
+
     if os.path.exists('history.csv'):
         os.remove('history.csv')
         print('История очищена\n')
@@ -152,14 +172,15 @@ def delete_history() -> None:
         print('\nУ вас не было истории, вам нечего очищать...\n')
 
 
-def action(number: int) -> None:
+def action(user_input: int) -> None:
     """
     Функция выполняет действие, указанное пользователем
 
-    :param number: Номер действия
+    :param user_input: Номер действия
     :return: None
     """
-    if number == 1:
+
+    if user_input == 1:
         city = get_user_city()
         all_weather_data = get_weather_data_for_city(city)
         weather_data = parse_weather_data(all_weather_data)
@@ -167,8 +188,9 @@ def action(number: int) -> None:
 
         print(weather_report(weather_data))
 
-    elif number == 2:
+    elif user_input == 2:
         city = input('\nВведите название города\n').strip()
+
         try:
             all_weather_data = get_weather_data_for_city(city)
             weather_data = parse_weather_data(all_weather_data)
@@ -179,19 +201,19 @@ def action(number: int) -> None:
         except KeyError:
             print('\nВведенного города нет в базе\n')
 
-    elif number == 3:
+    elif user_input == 3:
         history = create_or_read_history()
         if history.shape[0] == 0:
             print('\nВаша история пуста\n')
 
         else:
-            n = int(input('\nВведите число запросов, которые вы хотите увидеть\n'))
-            see_last_n_requests(n)
+            number_of_requests = int(input('\nВведите число запросов, которые вы хотите увидеть\n'))
+            see_last_n_requests(number_of_requests)
 
-    elif number == 4:
+    elif user_input == 4:
         delete_history()
 
-    elif number == 5:
+    elif user_input == 5:
         raise Exit("Попутного ветра!")
 
     else:
@@ -208,7 +230,6 @@ def interface() -> None:
     while True:
         try:
             user_input = int(input(TEXT_OF_INTERFACE).strip())
-
             action(user_input)
 
         except ValueError:
@@ -219,9 +240,11 @@ def interface() -> None:
             break
 
 
+
 class Exit(Exception):
     def __init__(self, message):
         super().__init__(message)
+
 
 
 interface()
